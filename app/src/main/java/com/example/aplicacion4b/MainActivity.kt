b59587b056f8335aa0d40304a0b0acf842671929
Pablo <pablo03mg@gmail.com@gmail.com>
2024-11-05 21:08:43 +0100

    Version Final

--- app/src/main/java/com/example/aplicacion4b/MainActivity.kt
+++ app/src/main/java/com/example/aplicacion4b/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.aplicacion4b
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.AlarmClock
 import androidx.appcompat.app.AppCompatActivity
 import com.example.aplicacion4b.databinding.ActivityMainBinding
 
@@ -12,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,12 +21,30 @@ class MainActivity : AppCompatActivity() {
         val sharedPreferences: SharedPreferences = getSharedPreferences("PreferenciasUsuario", MODE_PRIVATE)
         binding.nombreSaludo.text = sharedPreferences.getString("nombre_usuario", "Usuario")
 
-        binding.bttConfiguracion.setOnClickListener(){
+        binding.bttConfiguracion.setOnClickListener {
             startActivity(Intent(this, DatosUsuarioActivity::class.java))
         }
 
-        binding.telefono.setOnClickListener(){
+        binding.telefono.setOnClickListener {
             startActivity(Intent(this, ConfiguracionTelefono::class.java))
         }
+
+        binding.alarma.setOnClickListener {
+            val intent = Intent(AlarmClock.ACTION_SHOW_ALARMS)
+            startActivity(intent)
+        }
+
+        binding.tiempo.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.aemet.es/es/eltiempo/prediccion/municipios/jaen-id23050"))
+            startActivity(intent)
+        }
+
+        binding.mensajes.setOnClickListener(){
+            val intent = Intent(Intent.ACTION_MAIN).apply {
+                addCategory(Intent.CATEGORY_APP_MESSAGING)
+            }
+            startActivity(intent)
+        }
+
     }
 }
